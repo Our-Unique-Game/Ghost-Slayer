10169ed5a92873068d95f61589b5c51f30dfe08f
Gidi Rabi <118205453+GidiRabi@users.noreply.github.com>
2024-12-23 14:58:21 +0200

    linter fixes

--- Assets/Scripts/Cycle.cs
+++ Assets/Scripts/Cycle.cs
@@ -4,16 +4,20 @@ using UnityEngine;
  * This component represents a cycle of points in space.
  * It can represent, for example, a patrol path for an AI.
  */
-public class Cycle : MonoBehaviour {
+public class Cycle : MonoBehaviour
+{
     [SerializeField] float gizmoRadius = 0.2f;  // for gizmos of patrol points
 
-    private void OnDrawGizmos() {
+    private void OnDrawGizmos()
+    {
         Gizmos.color = Color.red;
 
         // Only draw gizmos if there are child points
-        if (transform.childCount > 0) {
+        if (transform.childCount > 0)
+        {
             // Loop through each child point and draw gizmos for visualization
-            for (int i = 0; i < transform.childCount; ++i) {
+            for (int i = 0; i < transform.childCount; ++i)
+            {
                 Transform currentPoint = transform.GetChild(i);
 
                 // Draw each point as a sphere
